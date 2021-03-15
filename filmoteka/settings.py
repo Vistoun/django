f4623820db1925fb5abd7f4adc9b9341c8b1c2e6
Vašek Lacheta <47534064+Vistoun@users.noreply.github.com>
2021-03-15 16:26:03 +0100

    url mapovani

--- filmoteka/settings.py
+++ filmoteka/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'movies',
 ]
 
 MIDDLEWARE = [
@@ -76,7 +77,7 @@ WSGI_APPLICATION = 'filmoteka.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': (BASE_DIR / 'db.sqlite3'),
     }
 }
 
@@ -103,9 +104,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/3.1/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'cs'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Eurupe/Prague'
 
 USE_I18N = True
 
--- filmoteka/urls.py
+++ filmoteka/urls.py
@@ -1,3 +1,28 @@
+# Import modulu admin z balíku django.contrib
+from django.contrib import admin
+# Import modulu path z balíku django.urls
+from django.urls import path
+# Import metody include() -pomocí ní připojíme cesty nastavené v souboru urls.py, který je součástí aplikace movies
+from django.urls import include
+# Třída RedirectView, která je součástí balíku django.views.generic umožňuje přesměrování pohledů (stránek)
+from django.views.generic import RedirectView
+# Z balíku django.conf importujeme nastavení (settings)
+from django.conf import settings
+# Z balíku django.conf.urls.static importujeme metodu static(),
+# # pomocí které zajistíme obsluhu statických souborů (jen ve vývojové fázi aplikace)
+from django.conf.urls.static import static
+
+# Mapování URL adres webu -propojení URL s jejich obsluhou
+urlpatterns = [
+# URL "admin/" je namapováno na administrační rozhraní webu
+path('admin/', admin.site.urls),
+# URL "movies/" je namapováno na aplikaci movies a její vlastní soubor 'movies.urls'
+path('movies/', include('movies.urls')),
+# domovská stránka webu ('') je automaticky přesměrována na url z předešlého řádku ('movies')
+path('', RedirectView.as_view(url='movies/')),] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+# Metodou static() zajistíme správné mapování adres statických souborů webu (css, js, obrázků atd.).
+# Jejich umístění bude definováno konstantami STATIC_URL a STATIC_ROOT v souboru settings.py
+
 """filmoteka URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
